30702a2b89c85eb44d31273af75d6b3ed51d554c
Felipe Lima <8339493+felipeal@users.noreply.github.com>
2022-03-22 03:11:34 -0300

    Gray out values above SP

--- src/ui/MemoryRowStack.tsx
+++ src/ui/MemoryRowStack.tsx
@@ -9,14 +9,19 @@ function focusInput(row: number) {
 export default function MemoryRowStack({ row, address, voltaMachine }: { row: number, address: number, voltaMachine: Volta }) {
   const [value, setValue] = useState(String(voltaMachine.getStackValue(address)));
   const [isCurrentStackPos, setIsCurrentStackPos] = useState(voltaMachine.getSPValue() === address);
+  const [isAboveStackPos, setIsAboveStackPos] = useState(address > voltaMachine.getSPValue());
 
   useEffect(() => {
     // Restore values on machine change
     setValue(String(voltaMachine.getStackValue(address)));
     setIsCurrentStackPos(voltaMachine.getSPValue() === address);
+    setIsAboveStackPos(address > voltaMachine.getSPValue());
 
     // Event subscriptions
-    voltaMachine.subscribeToEvent("REG.SP", (newValue) => setIsCurrentStackPos(newValue === address));
+    voltaMachine.subscribeToEvent("REG.SP", (newValue) => {
+      setIsCurrentStackPos(Number(newValue) === address);
+      setIsAboveStackPos(address > Number(newValue));
+    });
     voltaMachine.subscribeToEvent(`STACK.${address}`, (newValue) => setValue(String(newValue)));
   }, [voltaMachine]);
 
@@ -25,7 +30,7 @@ export default function MemoryRowStack({ row, address, voltaMachine }: { row: nu
       <td className="monospace-font pc-sp-arrow">{isCurrentStackPos ? "→" : ""}</td>
       <td className="table-address">{address}</td>
       <td>
-        <input className="table-value" inputMode="numeric" value={value} onChange={(event) => {
+        <input className={`table-value ${isAboveStackPos ? "table-value-above-sp" : ""}`} inputMode="numeric" value={value} onChange={(event) => {
           setValue(String(event.target.value));
         }} onBlur={(event) => {
           voltaMachine.setStackValue(address, Number(event.target.value)); // Write value to stack on focus out
