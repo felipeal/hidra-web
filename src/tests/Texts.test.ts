60f28472e1f520149d45818d834999d9a649e8bc
Felipe Lima <8339493+felipeal@users.noreply.github.com>
2022-04-01 04:09:19 -0300

    Add coverage for instruction and directive texts

--- src/tests/Texts.test.ts
+++ src/tests/Texts.test.ts
@@ -1,3 +1,4 @@
+import { Assembler } from "../core/Assembler";
 import { Texts } from "../core/Texts";
 import { buildMachine, getMachineNames } from "../ui/MachineUtils";
 
@@ -8,9 +9,24 @@ describe("Texts", () => {
       const instructionFormats = machine.getInstructions().map(i => i.getAssemblyFormat());
       const descriptions = instructionFormats.map(f => Texts.getInstructionDescription(f, machine));
 
+      // No description should be empty
+      for (const [name, description] of descriptions) {
+        expect(name).not.toBeFalsy();
+        expect(description).not.toBeFalsy();
+      }
+
       // Each instruction for a specific machine should have a distinct name/description
-      expect(new Set(descriptions.map(d => d[0]).filter(d => d)).size).toEqual(instructionFormats.length);
-      expect(new Set(descriptions.map(d => d[1]).filter(d => d)).size).toEqual(instructionFormats.length);
+      expect(new Set(descriptions.map(d => d[0])).size).toEqual(instructionFormats.length);
+      expect(new Set(descriptions.map(d => d[1])).size).toEqual(instructionFormats.length);
+    }
+  });
+
+  test("directives: should have descriptions for every directive", () => {
+    for (const directive of [...Assembler.DIRECTIVES, "dab/daw"]) {
+      const description = Texts.getDirectiveDescription(directive);
+      expect(description.name).not.toBeFalsy();
+      expect(description.description).not.toBeFalsy();
+      expect(description.examples).not.toBeFalsy();
     }
   });
 
