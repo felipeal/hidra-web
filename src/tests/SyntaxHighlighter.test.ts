d3c7cceb89cb890d99fa719a583957123426833a
Felipe Lima <8339493+felipeal@users.noreply.github.com>
2022-04-17 20:08:52 -0300

    Add coverage for syntax highlighter

---
+++ src/tests/SyntaxHighlighter.test.ts
@@ -0,0 +1,90 @@
+import { } from "./utils/jsdomSetup";
+import { makeFunction_processToken } from "../ui/utils/SyntaxHighlighter";
+import { Neander } from "../core/machines/Neander";
+import CodeMirror from "codemirror";
+
+let processToken: (stream: CodeMirror.StringStream) => string | null;
+
+type TokenStyle = {token: string, style: string | null};
+
+function computeTokenStyles(line: string): TokenStyle[] {
+  const tokenStyles: TokenStyle[] = [];
+  const stream = new CodeMirror.StringStream(line);
+
+  while (stream.pos < line.length) {
+    const style = processToken(stream);
+    tokenStyles.push({ token: stream.current(), style });
+    stream.start = stream.pos;
+  }
+
+  return tokenStyles;
+}
+
+function revealStyles(code: string) {
+  let result = "";
+  for (const tokenStyle of computeTokenStyles(code)) {
+    result += tokenStyleToString(tokenStyle);
+  }
+  return result;
+}
+
+function tokenStyleToString({ token, style }: TokenStyle) {
+  switch (style) {
+    case "hidra-instruction": return `<i>${token}</i>`;
+    case "hidra-directive": return `<d>${token}</d>`;
+    case "hidra-string": return `<s>${token}</s>`;
+    case "hidra-comment": return `<c>${token}</c>`;
+    default: return token;
+  }
+}
+
+describe("Syntax Highlighter", () => {
+
+  beforeAll(() => {
+    processToken = makeFunction_processToken(new Neander());
+  });
+
+  it("instructions: should highlight when valid", () => {
+    expect(revealStyles("lda 0")).toBe("<i>lda</i> 0");
+    expect(revealStyles("LdA 0")).toBe("<i>LdA</i> 0"); // Case-insensitive
+    expect(revealStyles("POP 0")).toBe("POP 0"); // Different machine's instruction
+    expect(revealStyles("NOPX 0")).toBe("NOPX 0"); // Non-existent instruction
+    expect(revealStyles("Label: LDA 0")).toBe("Label: <i>LDA</i> 0"); // After label
+    expect(revealStyles("LDA 'LDA'")).toBe("<i>LDA</i> <s>'LDA'</s>"); // With string containing instruction
+  });
+
+  it("directives: should highlight when valid", () => {
+    expect(revealStyles("org 0")).toBe("<d>org</d> 0");
+    expect(revealStyles("dB")).toBe("<d>dB</d>"); // Case-insensitive
+    expect(revealStyles("dw;")).toBe("<d>dw</d><c>;</c>"); // With comment
+    expect(revealStyles("Label: dab 0")).toBe("Label: <d>dab</d> 0"); // With label
+    expect(revealStyles("daw 'daw'")).toBe("<d>daw</d> <s>'daw'</s>"); // With string containing directive
+  });
+
+  it("strings: should highlight finished and unfinished strings", () => {
+    expect(revealStyles(" 'a' ")).toBe(" <s>'a'</s> "); // Finished string
+    expect(revealStyles(" 'a ")).toBe(" <s>'a </s>"); // Unfinished string
+    expect(revealStyles("'a','b' 'c")).toBe("<s>'a'</s>,<s>'b'</s> <s>'c</s>"); // Unfinished strings after multiple
+  });
+
+  it("literal quotes: should be detected correctly and behave as strings", () => {
+    expect(revealStyles("''';")).toBe("<s>'''</s><c>;</c>"); // Literal quote
+    expect(revealStyles("'''';")).toBe("<s>'''';</s>"); // Unfinished string with literal quote
+    expect(revealStyles("''''';")).toBe("<s>'''''</s><c>;</c>"); // String with literal quote
+    expect(revealStyles("'''';';")).toBe("<s>'''';'</s><c>;</c>"); // String starting with literal quote
+    expect(revealStyles("';'''';")).toBe("<s>';''''</s><c>;</c>"); // String ending with literal quote
+    expect(revealStyles("''''''''''';")).toBe("<s>'''''''''''</s><c>;</c>"); // String with multiple literal quotes
+    expect(revealStyles("'''' ''' '''';")).toBe("<s>'''' ''' ''''</s><c>;</c>"); // String with spaced literal quotes
+  });
+
+  it("comments: should be detected correctly", () => {
+    expect(revealStyles("; a b")).toBe("<c>; a b</c>"); // Comment
+    expect(revealStyles(";; ;;")).toBe("<c>;; ;;</c>"); // Comment symbols inside comment
+    expect(revealStyles("NOP ;")).toBe("<i>NOP</i> <c>;</c>"); // Comment after instruction
+    expect(revealStyles("';'")).toBe("<s>';'</s>"); // Comment symbol inside string
+    expect(revealStyles("';")).toBe("<s>';</s>"); // Comment symbol inside unfinished string
+    expect(revealStyles("';';")).toBe("<s>';'</s><c>;</c>"); // Comment after string
+    expect(revealStyles(";';'")).toBe("<c>;';'</c>"); // String syntax inside comment
+  });
+
+});
--- src/ui/utils/SyntaxHighlighter.ts
+++ src/ui/utils/SyntaxHighlighter.ts
@@ -4,46 +4,47 @@ import CodeMirror from "codemirror";
 
 export function defineCodeMirrorMode(machine: Machine): void {
   CodeMirror.defineMode(machine.getName(), () => {
-    const instructions = machine.getInstructions().map(instruction => instruction.getMnemonic());
-    const directives = ["db", "dab", "dw", "daw", "org"]; // TODO: Move directives
-
     return {
-      token: function (stream: CodeMirror.StringStream, _state: unknown): string | null {
-        return processToken(stream, instructions, directives);
-      }
+      token: makeFunction_processToken(machine)
     };
   });
 }
 
-function processToken(stream: CodeMirror.StringStream, instructions: string[], directives: string[]): string | null {
-  // If semicolon
-  if (stream.eat(";")) {
-    stream.skipToEnd();
-    return "hidra-comment";
-  }
+export function makeFunction_processToken(machine: Machine): (stream: CodeMirror.StringStream) => string | null {
+  const instructions = machine.getInstructions().map(instruction => instruction.getMnemonic());
+  const directives = Assembler.DIRECTIVES;
 
-  // If word
-  if (stream.match(/\w+/)) {
-    if (arrayContains(instructions, stream.current())) {
-      return "hidra-instruction";
-    } else if (arrayContains(directives, stream.current())) {
-      return "hidra-directive";
-    } else {
-      return null;
+  return (stream: CodeMirror.StringStream) => {
+    // If semicolon
+    if (stream.eat(";")) {
+      stream.skipToEnd();
+      return "hidra-comment";
     }
-  }
 
-  const stringMatch = stream.match(Assembler.STRING_PATTERN, false);
-  if (stringMatch) {
-    stream.match(`'${stringMatch[1]}'`);
-    return "hidra-string";
-  } else if (stream.eat("'")) { // Unclosed string
-    stream.skipToEnd();
-    return "hidra-string";
-  }
+    // If word
+    if (stream.match(/\w+/)) {
+      if (arrayContains(instructions, stream.current())) {
+        return "hidra-instruction";
+      } else if (arrayContains(directives, stream.current())) {
+        return "hidra-directive";
+      } else {
+        return null;
+      }
+    }
 
-  stream.next();
-  return null;
+    const stringMatch = stream.match(Assembler.STRING_PATTERN, false);
+    if (stringMatch) {
+      stream.match(`'${stringMatch[1]}'`);
+      return "hidra-string";
+    } else if (stream.eat("'")) { // Unfinished string
+      stream.skipToEnd();
+      return "hidra-string";
+    }
+
+    stream.next();
+    return null;
+  };
+
 }
 
 function arrayContains(array: string[], needle: string): boolean {
